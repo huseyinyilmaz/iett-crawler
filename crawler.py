881a4932bb45f7250d6eeee99dce30cba9b5b8df
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2012-03-28 16:54:06 +0300

    fetching bus routes from another source

--- crawler.py
+++ crawler.py
@@ -21,36 +21,51 @@ BUSSTOP_DICT = OrderedDict()
 
 ERRORS = list()
 
+BUS_ROUTE_DICT = dict()
+
+
+def fetch_bus_routes():
+    r = requests.get('http://www.iett.gov.tr/ajaxh_kodu.php?getCountriesByLetters=1&letters=')
+    lst = list(filter(lambda x: x, r.text.split('|')))
+    # ['<b>38Z</b>:ZÜBEYDE HANIM MAH.-BEYAZIT',....]
+    for st in lst:
+        # str = '<b>38Z</b>:ZÜBEYDE HANIM MAH.-BEYAZIT'
+        num_raw,route = st.split(':',1)
+        route = route.strip()
+        num = bs4.BeautifulSoup(num_raw).getText()
+        BUS_ROUTE_DICT[num] = route
 
 def get_busstop_list(bus):
     """
     Get busstop list for given bus.
     """
+
     no = bus['code'].lower()
-    bs = bs4.BeautifulSoup(open(BUSSTOP_LIST_URL % no))
-    result = []
-    for item in bs.findAll('item'):
-        lng = item.find('geo:long').text
-        lat = item.find('geo:lat').text
-        item_key = (lng, lat)
-        if item_key in BUSSTOP_DICT:
-            # busstop on this location already entered.
-            # use existing one
-            busstop = BUSSTOP_DICT[item_key]
-        else:
-            busstop = {
-                'id': next(busstop_id_generator),
-                'long': item.find('geo:long').text,
-                'lat': item.find('geo:lat').text,
-                }
-            BUSSTOP_DICT[item_key] = busstop
+    with open(BUSSTOP_LIST_URL % no) as f:
+        bs = bs4.BeautifulSoup(f)
+        result = []
+        for item in bs.findAll('item'):
+            lng = item.find('geo:long').text
+            lat = item.find('geo:lat').text
+            item_key = (lng, lat)
+            if item_key in BUSSTOP_DICT:
+                # busstop on this location already entered.
+                # use existing one
+                busstop = BUSSTOP_DICT[item_key]
+            else:
+                busstop = {
+                    'id': next(busstop_id_generator),
+                    'long': item.find('geo:long').text,
+                    'lat': item.find('geo:lat').text,
+                    }
+                BUSSTOP_DICT[item_key] = busstop
 
-        result.append({
-            'title': item.title.text,
-            'descrition': item.description.text,
-            'location_id': busstop['id']})
+            result.append({
+                    'title': item.title.text,
+                    'descrition': item.description.text,
+                    'location_id': busstop['id']})
 
-    return result
+        return result
 
 
 def get_bus_iter():
@@ -69,7 +84,8 @@ def get_bus_iter():
 
             bus = {'code': code,
                    'no': no,
-                   'route': route}
+                   'route': BUS_ROUTE_DICT[no] if no in BUS_ROUTE_DICT else route }
+
             try:
                 bus['busstops'] = get_busstop_list(bus)
             except IOError:
@@ -97,4 +113,5 @@ def pull_data():
     print(json.dumps(result))
 
 if __name__ == '__main__':
+    fetch_bus_routes()
     pull_data()
